0dd61dd46b0f2334d6aa1b8fbe5048ba2cb2c633
andrew-cowick <andrew.cowick@bobcats.gcsu.edu>
2020-01-23 16:29:59 -0500

    Revert "Initial GUI commit"
    This reverts commit 0e580871

--- football-stattrack.py
+++ football-stattrack.py
@@ -1,24 +1,3 @@
-from tkinter import *
-from tkinter import messagebox
-
-root = Tk()
-root.title("Football Stat Track")
-root.geometry("650x550")
-
-
-def func_exit():
-    m_box = messagebox.askquestion("Exit", "Are you sure you want to exit?")
-    if m_box == 'yes':
-        root.destroy()
-
-
-menu_bar = Menu(root)
-root.config(menu=menu_bar)
-file = Menu(menu_bar, tearoff=0)
-menu_bar.add_cascade(label='File', menu=file)
-file.add_command(label='Exit', compound=LEFT, command=func_exit)
-
-
 class Player:
 
     def _init_(self, fName, lName, num, height, weight, gpa, bench, squat, vert, fDash):
@@ -32,6 +11,3 @@ class Player:
         self.squat = squat
         self.vert = vert
         self.fDash = fDash
-
-
-root.mainloop()
