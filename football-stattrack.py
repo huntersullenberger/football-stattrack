c693faf397611b562503c4ec85bc231f5e54e13b
huntersullenberger <hunter.sullenberger@bobcats.gcsu.edu>
2020-01-23 15:38:30 -0500

    commit 1

--- football-stattrack.py
+++ football-stattrack.py
@@ -1,4 +1,13 @@
 class Player:
 
-    def _init_(self, name):
-        self.name = name
+    def _init_(self, fName, lName, num, height, weight, gpa, bench, squat, vert, fDash):
+        self.fName = fName
+        self.lName = lName
+        self.num = num
+        self.height = height
+        self.weight = weight
+        self.gpa = gpa
+        self.bench = bench
+        self.squat = squat
+        self.vert = vert
+        self.fDash = fDash
